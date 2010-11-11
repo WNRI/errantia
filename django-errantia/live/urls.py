e7b4533bb203b502bc0aea262c925d5805371401
Odin Hørthe Omdal <odin.omdal@gmail.com>
2010-11-11 22:17:41 +0000

    live: remove alternate template

--- django-errantia/live/urls.py
+++ django-errantia/live/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^(?P<conf_slug>[\w-]+)/$', 'live.views.show'),
-    (r'^2/(?P<conf_slug>[\w-]+)/$', 'live.views.show2'),
 )
--- django-errantia/live/views.py
+++ django-errantia/live/views.py
@@ -12,13 +12,3 @@ def show(request, conf_slug):
         template = 'live/conference-embed.html'
 
     return render_to_response(template, {'conf': conf, 'chat_id': chat.id})
-
-def show2(request, conf_slug):
-    conf = get_object_or_404(Conference, slug=conf_slug)
-    chat = Room.objects.get_or_create(conf)
-
-    template = 'live/conference2.html'
-    if 'embed' in request.GET:
-        template = 'live/conference-embed.html'
-
-    return render_to_response(template, {'conf': conf, 'chat_id': chat.id})
