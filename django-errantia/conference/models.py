3432996eab74bd83ace0c54c0ff3e254fe43ed1f
Odin Hørthe Omdal <odin.omdal@gmail.com>
2010-11-14 16:48:48 +0100

    Talk: Add field state, to keep track of where we are

--- django-errantia/conference/models.py
+++ django-errantia/conference/models.py
@@ -8,10 +8,18 @@ class Conference(models.Model):
     def __unicode__(self):
         return self.title
 
+
+STATES = (
+  ('wait', 'Waiting'),
+  ('rec', 'Recording'),
+  ('done', 'Archived'),
+)
+
 class Talk(models.Model):
     title = models.CharField(max_length=64)
     slug = models.SlugField()
     conference = models.ForeignKey(Conference)
+    state = models.CharField(max_length=4, choices=STATES, default="wait")
 
     class Meta:
         unique_together = (("slug", "conference"),)
