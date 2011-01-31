7d8b275330572722977fe6d8debf1e1985c9dcd0
Odin Hørthe Omdal <odin.omdal@gmail.com>
2011-01-31 17:03:19 +0100

    conf: Fix url syntax error

--- django-errantia/conference/urls.py
+++ django-errantia/conference/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^(?P<conf_slug>[\w-]+)/(?P<talk_slug>[\w-]+)/$',
         'conference.views.show_talk', name='errantia:show_talk'),
-    (r'^(?P<conf_slug>[\w-]+)/$', 'conference.views.show_conf',
+    url(r'^(?P<conf_slug>[\w-]+)/$', 'conference.views.show_conf',
         name='errantia:show_conf'),
 )
