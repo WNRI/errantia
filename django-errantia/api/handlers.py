1a1a6a7dbdf00603a307dff0530e4341ce4d0ec8
Odin Hørthe Omdal <odin.omdal@gmail.com>
2011-03-20 21:15:49 +0100

    api: Conference-handler

--- django-errantia/api/handlers.py
+++ django-errantia/api/handlers.py
@@ -1,8 +1,9 @@
 from piston.handler import BaseHandler
 from piston.utils import rc, FormValidationError
 
-from conference.models import Talk
+from conference.models import Talk, Conference
 from slide.models import Slide, SlideForm
+from jchat.models import Room, Message
 
 class SlideHandler(BaseHandler):
     model = Slide
@@ -31,3 +32,27 @@ class SlideNewestHandler(BaseHandler):
         obj = Slide.objects.filter(talk__conference__slug=conf_slug).order_by("-added")[0]
 
         return obj
+
+class ConfHandler(BaseHandler):
+    allowed_methods = ('GET',)
+
+    def read(self, request, conf_slug):
+        conf = Conference.objects.get(slug=conf_slug)
+
+        try:
+            talks = Talk.objects.filter(conference__slug=conf_slug) \
+                    .order_by("-starting", "-added")
+            slides = Slide.objects.filter(talk=talks[0]).order_by("-added")[0]
+        except IndexError:
+            talks = {}
+            slides = {}
+
+        room = Room.objects.get_or_create(conf)
+        #room.msgs = room.messages()
+
+        return {
+                'conf':conf,
+                'talks': talks,
+                'slides': slides,
+                'room': room,
+                }
--- django-errantia/api/urls.py
+++ django-errantia/api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
-from api.handlers import SlideHandler, SlideNewestHandler
+from api.handlers import SlideHandler, SlideNewestHandler, ConfHandler
 
+conf_handler = Resource(ConfHandler)
 slide_handler = Resource(SlideHandler)
 slide_newest_handler = Resource(SlideNewestHandler)
 
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
    url(r'^slide/new/(?P<conf_slug>[^/]+)/', slide_newest_handler),
    url(r'^slide/(?P<talk__conference__slug>[^/]+)/(?P<talk__slug>[^/]+)/', slide_handler),
    url(r'^slide/(?P<talk__conference__slug>[^/]+)/', slide_handler),
+   url(r'^conf/(?P<conf_slug>[^/]+)/', conf_handler),
 )
